0cdc07e48653f3b1409b47cb374fc929382856ee
suhaillahmad <suhaillahmadd0@gmail.com>
2022-10-20 18:45:39 +0530

    Added Category

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Notes
+from .models import Category
 # Register your models here.
 
 admin.site.register(Notes)
+admin.site.register(Category)
--- base/models.py
+++ base/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
 
 class Notes(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     description = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
